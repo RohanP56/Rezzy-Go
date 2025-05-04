34e433176eaa954089d630525f94c542440d5378
Rohan <devx.rohan@gmail.com>
2025-05-04 08:06:39 +0530

    Refactor and enhance components: - Removed Hero component and its styles. - Added MailList and PropertyList components with corresponding styles. - Updated Navbar and Header components for consistency. - Implemented SearchItem component for hotel listings. - Revamped Hotel and Hotels pages to integrate new components and improve layout. - Adjusted CSS styles across various components for better UI/UX.

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,13 +1,13 @@
 import "./navbar.css"
 
-function Navbar() {
+const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
         <span className="logo">Rezzy Go</span>
         <div className="navItems">
-            <button className="navButton">Register</button>
-            <button className="navButton">Login</button>
+          <button className="navButton">Register</button>
+          <button className="navButton">Login</button>
         </div>
       </div>
     </div>
